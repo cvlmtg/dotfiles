ea57679e1324603ae4bfb18e0c7c5c8cd478423c
Matteo Cavalleri <cvlmtg@gmail.com>
2018-08-31 22:50:29 +0200

    get rid of pygmentize and try bat

--- config/fish/functions/less.fish
+++ config/fish/functions/less.fish
@@ -1,16 +1,7 @@
 function less
-  set -l OPEN '| pygmentize -f 256 -O encoding=utf-8,style=monokai "%s"'
-  set -l MATCH (string match -iq '*.min.*' -- "$argv"; echo $status)
-  set -l COUNT (count $argv)
-  set -x LESS '-R -i'
-
-  if test $MATCH -eq 0
-    command less $argv
+  if which bat > /dev/null
+    command env BAT_PAGER="less -Ri" bat --theme=1337 $argv
   else
-    if which pygmentize > /dev/null
-      env LESSOPEN="$OPEN" command less $argv
-    else
-      command less $argv
-    end
+    command less -i $argv
   end
 end
--- config/fish/functions/rg.fish
+++ config/fish/functions/rg.fish
@@ -1,8 +1,14 @@
 # tr serve perché sotto tmux appaiono dei caratteri
 # di controllo che non si vedono nelle shell normali
 function rg
+  if which bat > /dev/null
+    set PAGER "bat -p"
+  else
+    set PAGER "less -R"
+  end
+
   if isatty stdout
-    command rg --smart-case --heading --pretty -g "!*.min.js" -g "!*.map" $argv | tr -d "\017" | less -R
+    command rg --smart-case --heading --pretty -g "!*.min.js" -g "!*.map" $argv | tr -d "\017" | eval $PAGER
   else
     command rg $argv
   end
