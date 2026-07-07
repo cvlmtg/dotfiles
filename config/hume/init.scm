d11876c3e5e94c4798907c0ae1ebc5eece143f8a
Matteo Cavalleri <cvlmtg@gmail.com>
2026-07-07 10:20:56 +0200

    add hume config

---
+++ config/hume/init.scm
@@ -0,0 +1,23 @@
+(load-plugin "core:plum")
+(load-plugin "core:stdlib")
+(load-plugin "core:vim-keybind")
+
+(define-command! "move-down-select-word"
+  "Move down one visual line, then select the nearest word on that line."
+  (lambda (count extend)
+    (call! "move-down" count extend)
+    (call! "select-word-nearest-on-line" 1 extend)))
+
+(define-command! "move-up-select-word"
+  "Move up one visual line, then select the nearest word on that line."
+  (lambda (count extend)
+    (call! "move-up" count extend)
+    (call! "select-word-nearest-on-line" 1 extend)))
+
+(bind-keys! 'normal
+            ("j" "move-down-select-word")
+            ("k" "move-up-select-word")
+            ("down" "move-down-select-word")
+            ("up" "move-up-select-word"))
+
+(set-option! "theme" "ember")
