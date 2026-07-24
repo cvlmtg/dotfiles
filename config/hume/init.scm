0888d71c3a0bddd0ec689e83f2061a568af18581
Matteo Cavalleri <cvlmtg@gmail.com>
2026-07-24 12:58:09 +0200

    update hume config

--- config/hume/init.scm
+++ config/hume/init.scm
@@ -1,7 +1,9 @@
 (load-plugin "core:stdlib")
+(load-plugin "core:pickers")
 (load-plugin "core:vim-keybind")
-(declare-plugin "core:plum")
 (declare-plugin "core:lsp")
+(declare-plugin "core:plum")
+(declare-plugin "core:steel-server")
 
 (define-command! "move-down-select-word"
   "Move down one visual line, then select the nearest word on that line."
@@ -15,6 +17,10 @@
     (call! "move-up" count extend)
     (call! "select-word-nearest-on-line" 1 extend)))
 
+(bind-key! 'normal "space space" "lsp-goto-definition")
+(bind-key! 'normal "space b" "picker-buffers")
+(bind-key! 'normal "space f" "picker-files")
+(bind-key! 'normal "space d" "lsp-hover")
 (bind-key! 'normal "\\" "goto-alternate-file")
 (bind-keys! 'normal
             ("j" "move-down-select-word")
